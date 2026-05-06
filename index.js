46f181335c84d8bfff5c4e8067fb4b53290bc6a3
Anderson Aguilar <anderson.aguilar@tecsup.edu.pe>
2026-05-06 12:08:35 -0500

    Agregamos package.json

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const http = require('http');
+
+function requestController() {
+    console.log("Bienvenidos al curso :v")
+}
+
+const server = http.createServer(requestController);
+
+const PORT = process.env.PORT;
+
+server.listen(PORT, function () {
+  console.log("Aplicacion corriendo en: " + PORT);
+});
