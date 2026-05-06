267a721250c874ce66517e0b9b412d66f5d0dfff
Anderson Aguilar <anderson.aguilar@tecsup.edu.pe>
2026-05-06 12:34:38 -0500

    Corrige Servidor para Render

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const http = require('http');
+const http = require("http");
 
-function requestController() {
-    console.log("Bienvenidos al curso :v")
-}
+const requestController = (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Bienvenidos al curso");
+};
 
 const server = http.createServer(requestController);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, function () {
-  console.log("Aplicacion corriendo en: " + PORT);
+server.listen(PORT, () => {
+  console.log("Aplicacion corriendo en:", PORT);
 });
